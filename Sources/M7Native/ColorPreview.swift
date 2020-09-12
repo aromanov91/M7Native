b50e34755ac405e29af049792cb5836656455423
Alexander Romanov <aromanov07@gmail.ru>
2020-09-12 09:12:06 +0300

    Apply colors

---
+++ Sources/M7Native/ColorPreview.swift
@@ -0,0 +1,20 @@
+//
+//  ColorPreview.swift
+//  M7NativePrewiev
+//
+//  Created by 18391981 on 12.09.2020.
+//
+
+import SwiftUI
+
+struct ColorPreview: View {
+    var body: some View {
+        Image("home")
+    }
+}
+
+struct ColorPreview_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorPreview()
+    }
+}
--- Sources/M7Native/M7Native.swift
+++ Sources/M7Native/M7Native.swift
@@ -1,3 +1,15 @@
-struct M7Native {
+import SwiftUI
+
+public struct M7Native {
     var text = "Hello, World!"
+    
+    
+}
+
+public class ColorManager {
+    
+    // create static variables for custom colors
+    public static var red = Color("Red")
+
+    //... add the rest of your colors here
 }
