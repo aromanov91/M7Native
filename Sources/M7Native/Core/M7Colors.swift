13670d60c552f6b455133ae31435f60e4fcce685
Alexander Romanov <aromanov07@gmail.ru>
2020-09-13 11:16:29 +0300

    Add core M7Colors, M7Fonts and component M7Text

---
+++ Sources/M7Native/Core/M7Colors.swift
@@ -0,0 +1,133 @@
+//
+//  M7Fonts.swift
+//  DSOneDemo
+//
+//  Created by Alexander Romanov on 16.05.2020.
+//  Copyright © 2020 romanov. All rights reserved.
+//
+
+import SwiftUI
+
+public class M7Colors {
+    
+    /// Primary
+    public static var primary: Color {
+        return Color("Primary")
+    }
+    
+    /// On Primary
+    public enum onPrimary {
+        
+        /// High Emphasis
+        public static var highEmphasis: Color {
+            return Color("OnPrimaryHighEmphasis")
+        }
+        
+        /// Medium Emphasis
+        public static var mediumEmphasis: Color {
+            return Color("OnPrimaryMediumEmphasis")
+        }
+        
+        /// Disabled
+        public static var disabled: Color {
+            return Color("OnPrimaryDisabled")
+        }
+    }
+//    
+    /// Background
+    public enum background {
+        
+        /// High Emphasis
+        public static var primary: Color {
+            return Color("BackgroundPrimary")
+        }
+        
+        /// Medium Emphasis
+        public static var secondary: Color {
+            return Color("BackgroundSecondary")
+        }
+
+        /// Disabled
+        public static var tertiary: Color {
+            return Color("BackgroundTertiary")
+        }
+    }
+    
+    /// On Background
+    public enum onBackground {
+        
+        /// High Emphasis
+        public static var highEmphasis: Color {
+            return Color("OnBackgroundHighEmphasis")
+        }
+        
+        /// Medium Emphasis
+        public static var mediumEmphasis: Color {
+            return Color("OnBackgroundMediumEmphasis")
+        }
+        
+        /// Disabled
+        public static var disabled: Color {
+            return Color("OnBackgroundDisabled")
+        }
+    }
+
+    /// Surface
+    public enum surface {
+        
+        /// High Emphasis
+        public static var primary: Color {
+            return Color("SurfacePrimary")
+        }
+        
+        /// Medium Emphasis
+        public static var secondary: Color {
+            return Color("SurfaceSecondary")
+        }
+        
+        /// Disabled
+        public static var tertiary: Color {
+            return Color("SurfaceTertiary")
+        }
+    }
+    
+    /// On Surface
+    public enum onSurface {
+        
+        /// High Emphasis
+        public static var highEmphasis: Color {
+            return Color("OnSurfaceHighEmphasis")
+        }
+        
+        /// Medium Emphasis
+        public static var mediumEmphasis: Color {
+            return Color("OnSurfaceMediumEmphasis")
+        }
+        
+        /// Disabled
+        public static var disabled: Color {
+            return Color("OnSurfaceDisabled")
+        }
+    }
+    
+    /// Error
+    public static var error: Color {
+        return Color("Error")
+    }
+    
+    /// Success
+    public static var success: Color {
+        return Color("Success")
+    }
+    
+    /// Warning
+    public static var warning: Color {
+        return Color("Warning")
+    }
+    
+    
+    /// Link
+    public static var link: Color {
+        return Color("Primary")
+    }
+}
---
+++ Sources/M7Native/Core/M7Fonts.swift
@@ -0,0 +1,33 @@
+//
+//  M7Fonts.swift
+//  
+//
+//  Created by Alexander Romanov on 13.09.2020.
+//
+
+import SwiftUI
+
+public class M7Fonts {
+    
+    public static var largeTitle: Font { return Font.system(size: 34, weight: .heavy) }
+    
+    public static var title1:     Font { return Font.system(size: 28, weight: .heavy) }
+    
+    public static var title2:     Font { return Font.system(size: 22, weight: .bold) }
+    
+    public static var title3:     Font { return Font.system(size: 30, weight: .bold) }
+    
+    public static var subtitle1:  Font { return Font.system(size: 16, weight: .semibold) }
+    
+    public static var subtitle2:  Font { return Font.system(size: 14, weight: .regular) }
+    
+    public static var paragraph1: Font { return Font.system(size: 16, weight: .regular) }
+    
+    public static var paragraph2: Font { return Font.system(size: 14, weight: .regular) }
+    
+    public static var caption:    Font { return Font.system(size: 12, weight: .medium) }
+    
+    public static var overline:   Font { return Font.system(size: 12, weight: .bold).lowercaseSmallCaps() }
+    
+    public static var button:     Font { return Font.system(size: 16, weight: .bold) }
+}
